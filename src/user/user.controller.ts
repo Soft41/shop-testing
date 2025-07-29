5a53a928e395b0ab3a4b96ca9e14e6520f581c62
torgovec <arostar197541@gmail.com>
2025-07-29 11:02:18 +0300

    feat: migration mechanism

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,7 +9,6 @@ import {
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { plainToInstance } from 'class-transformer';
 import { PaginatedUserResponseDto, UserResponseDto } from './dto/response/user';
 import { ApiResponse } from '@nestjs/swagger';
 import { getUserSummary } from '../common/summary.helper';
